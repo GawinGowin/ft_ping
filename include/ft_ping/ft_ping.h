6a5d3059b7d3181b237fb07e0a8467858e493016
GawinGowin <you@example.com>
2026-04-18 17:40:51 +0900

    add: 公開API ヘッダーを追加

---
+++ include/ft_ping/ft_ping.h
@@ -0,0 +1,48 @@
+#ifndef FT_PING_H
+#define FT_PING_H
+
+#include <stdint.h>
+
+/* 重複検出システム */
+#define MAX_DUP_CHK 0x10000 // 65536個のシーケンス番号を追跡
+#define BITMAP_SHIFT 6      // 64bit単位でのビット操作用
+
+typedef uint64_t bitmap_t;
+
+struct rcvd_table {
+  bitmap_t bitmap[MAX_DUP_CHK / (sizeof(bitmap_t) * 8)];
+};
+
+/* ビット操作マクロ */
+#define A(tbl, bit) ((tbl)->bitmap[(bit) >> BITMAP_SHIFT])
+#define B(bit) (((bitmap_t)1) << ((bit) & ((1 << BITMAP_SHIFT) - 1)))
+
+typedef struct ftping_session t_ftping_session; // 前方宣言
+
+typedef struct ftping_config {
+  int datalen;
+  int ttl;
+  int tos;
+  long npackets;
+  int interval;
+  unsigned int opt_verbose;
+  unsigned int opt_adaptive;
+} t_ftping_config;
+
+typedef struct ftping_stats {
+  int ntransmitted;
+  int nreceived;
+  long tmin;
+  long tmax;
+  double tsum;
+  rcvd_table rcvd_tbl;
+} t_ftping_stats;
+
+void ftping_init();
+void ftping_run();
+void ftping_get_stats();
+void ftping_stop();
+void ftping_cleanup();
+void ftping_strerror();
+
+#endif /* FT_PING_H */
